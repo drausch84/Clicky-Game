aa26a4f17185e2c09189ca6f28799117ba4ddd32
David Rausch <David@Davids-MacBook-Pro-2.local>
2018-06-27 23:15:41 -0600

    fixed some css

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
     this.setState({
       currentScore: 0,
       topScore: this.state.topScore,
-      rightWrong: "Dude You Suck!",
+      rightWrong: "I'm A Saaaaaad Panda!",
       clicked: []
     });
     this.handleShuffle();
